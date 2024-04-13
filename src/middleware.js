ba82f96e8e8b7f7c0af47e0b343ecda4e9ebb466
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-04-13 16:40:05 +0530

    reset to previous accesstoken in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    // const accessToken = request.cookies.get("accessToken")?.value;
-    const accessToken = localStorage.getItem("accessToken");
+    const accessToken = request.cookies.get("accessToken")?.value;
+    
   
     const loggedUserRestrictedPaths =
       request.nextUrl.pathname === "/login" ||
